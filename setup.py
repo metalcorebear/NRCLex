e364265ea5005af77b392bc128e9b166399707b8
Riyadh Sharif <riyadh.sharif@grabtaxi.com>
2022-07-29 12:26:05 +0800

    remove collection

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/riyadhctg/NRCLex",
     packages=setuptools.find_packages(),
-    install_requires=['textblob', 'collections'],
+    install_requires=['textblob'],
     include_package_data=True,
     py_modules=["nrclex", "__init__"],
     classifiers=[
